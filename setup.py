acca6fc752cdf936fa99618cab3d9d916f6cfd35
Aitor Oses <aitor.oses@gmail.com>
2024-08-22 15:04:35 +0200

    chore: Bump version to 0.2.0 in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='natural-lens',
-    version='0.1.0',
+    version='0.2.0',
     description='An drive CLI tool for downloading database schemas and generating profiles.',
     author='Aitor Oses',
     author_email='aitor.oses@example.com',
